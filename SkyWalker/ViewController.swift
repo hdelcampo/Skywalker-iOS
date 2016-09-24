06c73702af5c5f4178e6640637296623e9660d46
FoxeyYy <hectorcampop@outlook.com>
2016-09-24 12:03:02 +0200

    Initial Commit

---
+++ SkyWalker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkyWalker
+//
+//  Created by Héctor Del Campo Pando on 24/9/16.
+//  Copyright © 2016 Héctor Del Campo Pando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
