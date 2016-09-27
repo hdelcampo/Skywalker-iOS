f32e868a9d76f2c55c833d2906d59f9e9d510fcc
FoxeyYy <hectorcampop@outlook.com>
2016-09-27 20:13:45 +0200

    Added icons, camera and preparations for overlay

---
+++ SkyWalker/OverlayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OverlayViewController.swift
+//  SkyWalker
+//
+//  Created by Héctor Del Campo Pando on 27/9/16.
+//  Copyright © 2016 Héctor Del Campo Pando. All rights reserved.
+//
+
+import UIKit
+
+class OverlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
